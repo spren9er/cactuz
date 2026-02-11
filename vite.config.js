04dd82d7a9a27eb37953943293b8cfef9fd773f9
Torsten Sprenger <mail@spren9er.de>
2026-02-11 12:49:30 +0100

    Update vite config to add tests

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [sveltekit(), devtoolsJson()] });
+export default defineConfig({
+  plugins: [sveltekit(), devtoolsJson()],
+  test: {
+    environment: 'jsdom',
+    alias: {
+      $lib: new URL('./src/lib', import.meta.url).pathname,
+    },
+  },
+});
